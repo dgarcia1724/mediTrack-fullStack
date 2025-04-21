e01d2053136a81095005ef9a2d7328d5e7cd1d18
Danny Garcia <dgarcia1724@berkeley.edu>
2025-04-21 15:13:38 -0700

    Added Firebase init file in config folder

---
+++ backend/auth-service/src/main/java/com/meditrack/auth_service/config/FirebaseInitializer.java
@@ -0,0 +1,28 @@
+package com.meditrack.auth_service.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Component
+public class FirebaseInitializer {
+
+    @PostConstruct
+    public void initialize() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
+} 
