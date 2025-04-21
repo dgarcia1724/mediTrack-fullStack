6657f30424adfbb8ff18c49e8574d8583dbc9a53
Danny Garcia <dgarcia1724@berkeley.edu>
2025-04-21 14:49:47 -0700

    added dependencies for auth service

---
+++ backend/auth-service/src/test/java/com/meditrack/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditrack.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
